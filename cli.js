650292412c1360cab17fb9343c857a43b0a216dd
Andrew Carpenter <andrew@audioinhd.com>
2020-11-18 22:35:04 -0500

    Handle help on commands, err, command

--- cli.js
+++ cli.js
@@ -5,11 +5,10 @@ const rawArgs = process.argv.slice(2)
 const commands = require('./index')
 const cmdMap = (c) => `${c.info.command} - ${c.info.description}`
 
-const cmd = async () => {
-  console.log(`Commands:\n${Object.values(commands).map(cmdMap).join('\n')}\n`)
-}
-
-const command = commands[rawArgs[0]] || { cmd }
+const cmdList = Object.values(commands).map(cmdMap).join('\n')
+const printCmds = `Commands:\n\n${cmdList}\n`
+const cmd = async () => console.log(printCmds)
+const command = commands[rawArgs[0]] || { cmd, info: { help: printCmds } }
 
 const main = async () => {
   if (rawArgs.includes('--help') || rawArgs.includes('-h')) {
